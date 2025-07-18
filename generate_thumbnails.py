1258de24711c0ff83d6aab45f98e139f5afbc838
Andrew Shen <andrewshenrocks@gmail.com>
2025-07-18 01:48:37 -0700

    using pillow and glob to generate the current images' thumbnails

--- generate_thumbnails.py
+++ generate_thumbnails.py
@@ -1,7 +1,15 @@
-from PIL import Image
 import os
+import glob
+from PIL import Image
 
-images_directory = "images"
-thumbnail_directory = "thumbnails"
 thumbnail_size = (300, 300)
 
+years = ["2023", "2024", "2025"]
+
+for year in years:
+    images = glob.glob(f"images/{year}/*.JPG")
+    for image in images:
+        relative_path = os.path.relpath(image, "images")
+        im = Image.open(image)
+        im.thumbnail(thumbnail_size)
+        im.save(f"thumbnails/{relative_path}")
