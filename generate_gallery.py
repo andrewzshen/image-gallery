1b60e3a08d4d7c87c0b980d9612765c021a6cba2
Andrew Shen <andrewshenrocks@gmail.com>
2025-07-21 23:02:42 -0700

    refactored gallery generation code, adding arg parsing, no longer using glob

--- generate_gallery.py
+++ generate_gallery.py
@@ -1,26 +1,61 @@
+# file: generate_gallery.py
+# A script that generates the index.html file of the image gallery
+
+import argparse
 import os
 import glob
-from dominate import document
 from dominate.tags import *
 
-years = ["2023", "2024", "2025"]
+def generate_anchor(image, thumbnail):
+    anchor = a(href=image, target="_blank")
+    anchor.add(img(src=thumbnail, alt="", loading="lazy"))
+    return anchor
 
-doc = document("Balls")
+def generate_gallery(image_dir, thumb_dir):
+    # this function assumes that the iamge and its respective thumbnail have the same name
+    _div = div(_class="gallery")
 
-with doc.head:
-    meta(charset="UTF-8")
-    link(rel="stylesheet", href="style.css")
+    images = os.listdir(image_dir)
 
-with doc:
-    for year in years:
-        h1(year)
-        with div(_class="gallery"):
-            images = glob.glob(f"images/{year}/*.JPG")
-            for image in images:
-                relative_path = os.path.relpath(image, "images")
-                thumbnail = f"thumbnails/{relative_path}"
-                with a(href=image, target="_blank"):
-                    img(src=thumbnail, alt="", loading="lazy")
+    for image_name in images:
+        image = os.path.join(image_dir, image_name)
+        thumbnail = os.path.join(thumb_dir, image_name)
+        _div.add(generate_anchor(image, thumbnail))
 
-with open("index.html", "w") as f:
-    f.write(doc.render())
+    return _div
+
+def main():
+    # parse args
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p", "--paths",
+        action="append", 
+        nargs=3,
+        metavar=("IMAGE_DIR", "THUMB_DIR", "DIV_TITLE"),
+        required=True,
+        help="Image and thumbnail directories, followed by the title of the gallery; repeatable."
+    )
+    args = parser.parse_args()
+
+    # main html document
+    _html = html()
+
+    # header
+    _header = _html.add(header())
+    _header.add(title("Balls"))
+    _header.add(meta(charset="UTF-8"))
+    _header.add(link(rel="stylesheet", href="style.css"))
+
+    # body
+    _body = _html.add(body())
+
+    for image_dir, thumb_dir, div_title in args.paths:
+        _body.add(h2(div_title))
+        _body.add(generate_gallery(image_dir, thumb_dir))
+
+    # write the contents to the file
+    with open("index.html", "w") as f:
+        f.write(_html.render())
+
+if __name__ == "__main__":
+    main()
