a409e284bc0b4285f1074c21b2ee32061923ac20
andrewzshen <andrewshenrocks@gmail.com>
2025-08-15 17:01:37 -0700

    updating index.html and tabs.js

--- tabs.js
+++ tabs.js
@@ -2,7 +2,7 @@ function openTab(event, tabName) {
     document.querySelectorAll(".tabcontent")
         .forEach(element => element.classList.remove("is-active"))
     
-    document.querySelectorAll(".tablinks")
+    document.querySelectorAll(".tablink")
         .forEach(button => button.classList.remove("active"))
     
     document.getElementById(tabName)?.classList.add("is-active")
